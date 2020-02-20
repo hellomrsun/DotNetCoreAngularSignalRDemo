459e03aa14e5dbdb828259141fab389fcb5e8cb2
hellomrsun <jiangong.sun@gmail.com>
2020-02-21 00:04:55 +0100

    api

---
+++ SignalrDotnetCoreApi/SignalrDotnetCoreApi/SignalrDotnetCoreApi/Models/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalrDotnetCoreApi.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ SignalrDotnetCoreApi/SignalrDotnetCoreApi/SignalrDotnetCoreApi/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace SignalrDotnetCoreApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Signalr .net core API",
+                    Version = "v1"
+                });
+            });
+
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API V1");
+                x.RoutePrefix = string.Empty;
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("chatHub");
+            });
+        }
+    }
+}
