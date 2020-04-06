56b6d42c49488d7e8b50ab60e2984b32b7ea9f60
hellomrsun <soleilcharles@gmail.com>
2020-04-06 11:46:52 +0200

    update

--- SignalrDotnetCoreApi/SignalrDotnetCoreApi/SignalrDotnetCoreApi.Database/Context/WineDbContext.cs
+++ SignalrDotnetCoreApi/SignalrDotnetCoreApi/SignalrDotnetCoreApi.Database/Context/WineDbContext.cs
@@ -8,30 +8,21 @@ namespace SignalrDotnetCoreApi.Database.Context
 {
     public class WineDbContext : DbContext, IDbContext
     {
+        private static string _codeFirstUpdateConnectionString;
         private static string CodeFirstUpdateConnectionString
         {
             get
             {
-                var config = new ConfigurationBuilder()
-                    .AddJsonFile(Path.Combine(Environment.CurrentDirectory, "appsettings.json"))
-                    .Build();
-                var result = config.GetSection("ConnectionStrings")["WineDbConnection"];
+                if (string.IsNullOrWhiteSpace(_codeFirstUpdateConnectionString))
+                {
+                    var config = new ConfigurationBuilder()
+                        .AddJsonFile(Path.Combine(Environment.CurrentDirectory, "appsettings.json"))
+                        .Build();
+                    _codeFirstUpdateConnectionString = config.GetSection("ConnectionStrings")["WineDbConnection"];
+                }
 
-                return result;
+                return _codeFirstUpdateConnectionString;
             }
-        }
-
-        public WineDbContext() : this(CodeFirstUpdateConnectionString)
-        {
-        }
-
-        public WineDbContext(string connectionString) : base(GetOptions(connectionString))
-        {
-        }
-
-        private static DbContextOptions GetOptions(string connectionString)
-        {
-            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
         }
 
         public DbSet<Grape> Grapes { get; set; }
@@ -46,6 +37,19 @@ namespace SignalrDotnetCoreApi.Database.Context
 #endif
 
             optionsBuilder.UseSqlServer(CodeFirstUpdateConnectionString);
+        }
+
+        public WineDbContext() : this(CodeFirstUpdateConnectionString)
+        {
+        }
+
+        public WineDbContext(string connectionString) : base(GetOptions(connectionString))
+        {
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
         }
     }
 }
